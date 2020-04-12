48fd3ce6985f06370070e3b8229d5d4b5ba74253
unknown <ogbemudiatimothy@gmail.com>
2020-04-12 16:25:53 +0100

    minor update

--- src/components/HomePage/Section1/Section1.js
+++ src/components/HomePage/Section1/Section1.js
@@ -31,10 +31,17 @@ const Section1 = () => {
               <div>
                 <p className={classes.section1Text}>
                   I'm a frontend focused fullstack developer passionate about
-                  React, Hooks, GraphQL and design. <br /> When i'm not coding
-                  or writing blog posts, i'm usually thrashing out licks on
-                  guitar a la Stevie Ray Vaughan or on Twitter or you could
-                  catch me on Youtube watching every and <br />
+                  React, Hooks, GraphQL and design. I'm also author of the{" "}
+                  <a
+                    href="https://www.npmjs.com/package/uwadie"
+                    target="_blank"
+                  >
+                    Uwadie
+                  </a>{" "}
+                  plugin <br /> When i'm not coding or writing blog posts, i'm
+                  usually thrashing out licks on guitar a la Stevie Ray Vaughan
+                  or on Twitter or you could catch me on Youtube watching every
+                  and <br />
                   anything from videos on the world war to The Weeknd live.
                 </p>
               </div>
